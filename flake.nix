0a221fc3dc4516b3dbc2c0cfd9a38df545393c0e
DADA30000 <vadimhack.ru@gmail.com>
2024-07-10 13:57:48 +0300

    adapting so that I could build straight from github

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    hyprland = {
+      type = "git";
+      url = "https://github.com/hyprwm/Hyprland";
+      submodules = true;
+    };
+    nvidia-patch = {
+      url = "github:icewind1991/nvidia-patch-nixos";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    spicetify-nix.url = "github:the-argus/spicetify-nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    kwin-effects-forceblur = {
+      url = "github:taj-ny/kwin-effects-forceblur";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland-plugins = {
+        url = "github:hyprwm/hyprland-plugins";
+        inputs.hyprland.follows = "hyprland"; # IMPORTANT
+    };
+    minegrub-world-sel-theme = {
+      url = "github:Lxtharia/minegrub-world-sel-theme";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pollymc.url = "github:fn2006/PollyMC";
+    pipewire-screenaudio.url = "github:IceDBorn/pipewire-screenaudio";
+    ags.url = "github:Aylur/ags";
+  };
+
+  outputs = {self, nixpkgs, home-manager, ...} @ inputs: let user = "l0lk3k"; hostname = "nixos"; in { # DON'T FORGET TO CHANGE USERNAME AND HOSTNAME HERE <<<<<<<<<<
+    nixosConfigurations."${hostname}" = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs user hostname; };
+      modules = [
+        ./nixos/configuration.nix
+	inputs.minegrub-world-sel-theme.nixosModules.default
+	home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+	      extraSpecialArgs = { inherit inputs; }; 
+              useGlobalPkgs = true;
+              users."${user}" = import ./nixos/home.nix;
+              useUserPackages = true;
+            };
+          }
+      ];
+    };
+  };
+}
--- update.sh
+++ update.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 if [ -f ./check ]; then
   mkdir nixos
-  rm -rf ./nixos/*
+  rm -rf ./*
   cp -r /etc/nixos/. ./nixos
   rm ./nixos/hardware-configuration.nix
   git add . --all
   git commit -m "lil update"
   git push -u
-  ( cd ~/.mozilla; ./update.sh )
-  #rclone sync -v /fileserver/Music google:Music
 else
   echo "change your working directory to dotfiles already"
 fi
