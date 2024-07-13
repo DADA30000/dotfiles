aa1bc76022aaa391c5273f4b9a206a72120dd5bf
DADA30000 <vadimhack.ru@gmail.com>
2024-07-13 21:18:33 +0300

    lil update

---
+++ start_dont_touch_it.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [ -f ./check ] &; then
+  #git clone https://github.com/DADA30000/mozilla.git ~/.mozilla
+  #sudo touch /password
+  #( echo "Введите пароль Nextcloud"; read; sudo chmod 777 /password; echo $REPLY >> /password; sudo chown nextcloud:nextcloud /password; sudo chmod 400 /password )
+  #sudo mkdir /fileserver
+  #git config --global credential.helper store
+  #sudo chown -R nginx:nginx /fileserver
+else
+  echo "change your working directory to dotfiles"
+fi
