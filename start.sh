6d1a02039f0c272ac93b4a5748ee777f5da4c209
DADA30000 <vadimhack.ru@gmail.com>
2024-07-13 21:23:39 +0300

    lil update

--- start.sh
+++ start.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 if [ -f ./check ]; then
   sudo find /mnt/etc/nixos ! -name 'hardware-configuration.nix' -type f -exec rm -rf {} +
-  sudo cp -r ./nixos/* /mnt/etc/nixos
-  sudo nixos-install --flake "/mnt/etc/nixos#nixosConfigurations.nixos"
+  sudo rm ./nixos/hardware-configuration.nix
+  sudo cp -r ./nixos flake.{nix,lock} /mnt/etc/nixos
+  sudo nixos-install --flake "/mnt/etc/nixos#nixos"
+  #sudo nixos-install --flake github:DADA30000/dotfiles#nixos
 else
   echo "change your working directory to dotfiles"
 fi
+
+##!/usr/bin/env bash
+#if [ -f ./check ] &; then
+#  git clone https://github.com/DADA30000/mozilla.git ~/.mozilla
+#  sudo touch /password
+#  ( echo "Введите пароль Nextcloud"; read; sudo chmod 777 /password; echo $REPLY >> /password; sudo chown nextcloud:nextcloud /password; sudo chmod 400 /password )
+#  sudo mkdir /fileserver
+#  git config --global credential.helper store
+#  sudo chown -R nginx:nginx /fileserver
+#else
+#  echo "change your working directory to dotfiles"
+#fi
