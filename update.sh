8b87fcc13196639ee7c508afc32f235773ce92ce
DADA30000 <vadimhack.ru@gmail.com>
2024-07-19 07:05:47 +0300

    lil update

--- update.sh
+++ update.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 if [ -f ./check ]; then
-  mkdir nixos
   rm -rf ./nixos ./iso ./flake.nix ./flake.lock
   cp -r /etc/nixos/* ./
   git add . --all
-  git commit -m "iso update"
+  git commit -m "lil update"
   git push -u
 else
   echo "change your working directory to dotfiles already"
