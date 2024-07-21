68682efd8840a4792c32b3db690a071695a96a31
DADA30000 <vadimhack.ru@gmail.com>
2024-07-21 09:19:16 +0300

    iso update

--- iso/configuration.nix
+++ iso/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, modulesPath, var, ... }:
+{ pkgs, modulesPath, ... }:
 {
   imports = [
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
@@ -9,20 +9,6 @@
   networking.networkmanager.enable = true;
   networking.wireless.enable = false;
   time.timeZone = "Europe/Moscow";
-  boot.loader.grub = {
-    minegrub-world-sel = { 
-      enable = true;
-      customIcons = [{
-        name = "nixos";
-        lineTop = "NixOS (06/07/2024, 2:24 AM)";
-        lineBottom = "Creative Mode, Cheats, Version: unstable";
-        customImg = builtins.path {
-          path = ../nixos/stuff/nixos-img.png;
-          name = "nixos-img";
-        };
-      }];
-    };
-  }; 
   i18n.defaultLocale = "ru_RU.UTF-8";
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
@@ -35,116 +21,46 @@
       if [[ $EUID -ne 0 ]]; then
         exec sudo nix-install
       fi
+      setfont cyr-sun16
       clear
-      echo "Проверка наличия соединения с интернетом..."
+      echo -e "\e[34mПроверка наличия соединения с интернетом...\e[0m"
       if ! nc -zw1 google.com 443 > /dev/null 2>&1; then
-        echo "Соединение не установлено :("
+        echo -e "\e[31mСоединение не установлено :(\e[0m"
         if gum confirm "Настроить подключение?"; then
           nmtui
 	  if ! nc -zw1 google.com 443 > /dev/null 2>&1; then
-	    echo "Соединение не было установлено, перезапуск..."
+	    echo -e "\e[34mСоединение не было установлено, перезапуск...\e[0m"
 	    sleep 2;
 	    exec nix-install
 	  fi
 	fi
       fi
-      echo "Соединение установлено!"
-      sleep 2
+      echo -e "\e[32mСоединение установлено!\e[0m"
+      sleep 1
       url="https://github.com/DADA30000/dotfiles"
       clear
-      if gum confirm --default=false "Поменять URL репозитория с файлами конфигурации?"; then
+      if gum confirm --default=false "Поменять URL репозитория с файлами конфигурации? (скрипт запускает start.sh из репозитория, репозиторий должен быть публичным)"; then
         url=$(gum input --placeholder "Пример: https://github.com/DADA30000/dotfiles")
       fi
-      clear
-      echo -e "\e[34mВыберите диск на котором будет расположена \e[4;34mСИСТЕМА\e[0m"
-      echo -e "\e[32mСовет: вы всегда можете перезапустить скрипт нажав Ctrl+C (желательно не во время этапа установки)\e[0m"
-      sudo fdisk -l | grep -i -E "^(Диск|Disk|/)"; echo;
-      disk_system=$(sudo fdisk -l | grep -i -E "^Диск" | gum choose | grep -oE '/[^[:space:]]*:' | sed 's/\://g')
-      clear
-      if gum confirm --default=false "Настроить дополнительный диск? (Он будет смонтирован/расположен в /home/пользователь/Games и так же будет ОТФОРМАТИРОВАН, настраивать его НЕОБЯЗАТЕЛЬНО)"; then
-        echo -e "\e[34mВыберите диск, который будет использоваться как \e[4;34mДОПОЛНИТЕЛЬНЫЙ ДИСК\e[0m"
-	disk_games=$(sudo fdisk -l | grep -i -E "^Диск" | gum choose | grep -oE '/[^[:space:]]*:' | sed 's/\://g')
-      fi
-      clear
-      if [ -n "$disk_games" ]; then
-        echo "Вы хотите установить СИСТЕМУ на $disk_system, и использовать в качестве ДОПОЛНИТЕЛЬНОГО ДИСКА $disk_games (предупреждение: он отформатируется)"
-      else
-        echo "Вы хотите установить СИСТЕМУ на $disk_system"
-      fi
-      if gum confirm "Всё верно?"; then
+      if GIT_ASKPASS=true git ls-remote "$url" > /dev/null 2>&1; then
         clear
-        echo "Начинается установка, откиньтесь на спинку кресла и наслаждайтесь видом :)" | clolcat
-	sleep 2
-	echo -e "\e[34mРазметка дисков..."
-	echo "label: gpt" | sudo sfdisk "$disk_system"
-	echo "start=        2048, size=     1048576, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B" | sudo sfdisk "$disk_system"
-	echo "start=     1050624, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4" | sudo sfdisk "$disk_system" -N 2
-	if [ -n "$disk_games" ]; then
-	  echo "label: gpt" | sudo sfdisk "$disk_games"
-	  echo "type=0FC63DAF-8483-4772-8E79-3D69D8477DE4" | sudo sfdisk "$disk_games"
-	fi
-	echo -e "\e[34mФорматирование и монтирование разделов...\e[0m"
-	sudo mkdir -p /mnt
-	if [ $(echo "$disk_system" | grep -c nvme) -eq 1 ]; then
-	  sudo mkfs.fat -n boot -F 32 "''${disk_system}p1"
-	  sudo mkfs.btrfs -f -L nixos "''${disk_system}p2"
-	  sudo mount "''${disk_system}p2" /mnt
-	  sudo btrfs subvolume create /mnt/root
-	  sudo btrfs subvolume create /mnt/home
-	  sudo btrfs subvolume create /mnt/nix
-	  sudo umount /mnt
-	  sudo mount -o compress-force=zstd,subvol=root "''${disk_system}p2" /mnt
-	  sudo mkdir /mnt/{home,nix}
-	  sudo mount -o compress-force=zstd,subvol=home "''${disk_system}p2" /mnt/home
-	  sudo mount -o compress-force=zstd,noatime,subvol=nix "''${disk_system}p2" /mnt/nix
-	  sudo mkdir /mnt/boot
-	  sudo mount "''${disk_system}p1" /mnt/boot
-	else
-	  sudo mkfs.fat -n boot -F 32 "''${disk_system}1"
-	  sudo mkfs.btrfs -f -L nixos "''${disk_system}2"
-	  sudo mount "''${disk_system}2" /mnt
-	  sudo btrfs subvolume create /mnt/root
-	  sudo btrfs subvolume create /mnt/home
-	  sudo btrfs subvolume create /mnt/nix
-	  sudo umount /mnt
-	  sudo mount -o compress-force=zstd,subvol=root "''${disk_system}2" /mnt
-	  sudo mkdir /mnt/{home,nix}
-	  sudo mount -o compress-force=zstd,subvol=home "''${disk_system}2" /mnt/home
-	  sudo mount -o compress-force=zstd,noatime,subvol=nix "''${disk_system}2" /mnt/nix
-	  sudo mkdir /mnt/boot
-	  sudo mount "''${disk_system}1" /mnt/boot
-	fi
-	if [ -n "$disk_games" ]; then
-	  if [ $(echo "$disk_games" | grep -c nvme) -eq 1 ]; then
-	    sudo mkdir /mnt1
-	    sudo mkfs.btrfs -f -L Games "''${disk_games}p1"
-	    sudo mount "''${disk_games}p1" /mnt1
-	    sudo btrfs subvolume create /mnt1/games
-	    sudo umount /mnt1
-	    sudo rmdir /mnt1
-	    sudo mkdir -p /mnt/home/${var.user}/Games
-	  else
-	    sudo mkdir /mnt1
-	    sudo mkfs.btrfs -f -L Games "''${disk_games}1"
-	    sudo mount "''${disk_games}1" /mnt1
-	    sudo btrfs subvolume create /mnt1/games
-	    sudo umount /mnt1
-	    sudo rmdir /mnt1
-	    sudo mkdir -p /mnt/home/${var.user}/Games
-	  fi
-	fi
-	echo -e "\e[34mКопирование файлов конфигурации...\e[0m"
-	sudo mkdir /mnt1
-	sudo git clone "$url" /mnt1/dotfiles
-	sudo mkdir -p /mnt/etc/nixos
-	echo -e "\e[34mУстановка системы...\e[0m"
-	(cd /mnt1/dotfiles; ./start.sh)
-	sudo rm -rf /mnt1
-	echo "\e[32mУстановка завершена, перезагрузка через 10 секунд... (Ctrl+C для отмены)\e[0m"
-	sleep 10
-	reboot
+        echo -e "\e[34mКлонирование репозитория...\e[0m"
+        mkdir /mnt2
+        if git clone "$url" /mnt2/dotfiles; then
+          cd /mnt2/dotfiles
+          echo -e "\e[34mЗапуск start.sh...\e[0m"
+          exec ./start.sh
+        else
+          echo -e "\e[31mОшибка клонирования репозитория, перезапуск скрипта...\e[0m"
+          sleep 3
+          rm -rf /mnt2
+          exec nix-install
+        fi
+      else
+        echo -e "\e[31mURL репозитория неверный или приватный, перезапуск скрипта...\e[0m"
+	sleep 3
+	exec nix-install
       fi
-
     '')
   ];
 }
--- start.sh
+++ start.sh
@@ -1,12 +1,111 @@
 #!/usr/bin/env bash
 if [ -f ./check ]; then
-  sudo find /mnt/etc/nixos ! -name 'hardware-configuration.nix' -type f -exec rm -rf {} +
-  sudo rm ./nixos/hardware-configuration.nix
-  sudo nixos-generate-config --no-filesystems --root /mnt
-  sudo cp -r ./nixos ./iso flake.{nix,lock} /mnt/etc/nixos
-  sudo mv /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/nixos
-  sudo nixos-install -v --flake "/mnt/etc/nixos#nixos" --impure
-  #sudo nixos-install --flake github:DADA30000/dotfiles#nixos
+  clear
+  echo -e "\e[34mВыберите диск на котором будет расположена \e[4;34mСИСТЕМА\e[0m"
+  echo -e "\e[32mСовет: вы всегда можете перезапустить скрипт нажав Ctrl+C (желательно не во время этапа установки)\e[0m"
+  fdisk -l | grep -i -E "^(Диск|Disk|/)"; echo;
+  disk_system=$( fdisk -l | grep -i -E "^Диск" | gum choose | grep -oE '/[^[:space:]]*:' | sed 's/\://g')
+  clear
+  if gum confirm --default=false "Настроить дополнительный диск? (Он будет смонтирован/расположен в /home/пользователь/Games и так же будет ОТФОРМАТИРОВАН, настраивать его НЕОБЯЗАТЕЛЬНО)"; then
+    echo -e "\e[34mВыберите диск, который будет использоваться как \e[4;34mДОПОЛНИТЕЛЬНЫЙ ДИСК\e[0m"
+    disk_games=$( fdisk -l | grep -i -E "^Диск" | gum choose | grep -oE '/[^[:space:]]*:' | sed 's/\://g')
+  fi
+  clear
+  if [ -n "$disk_games" ]; then
+    echo "Вы хотите установить СИСТЕМУ на $disk_system, и использовать в качестве ДОПОЛНИТЕЛЬНОГО ДИСКА $disk_games (предупреждение: он отформатируется)"
+  else
+    echo "Вы хотите установить СИСТЕМУ на $disk_system"
+  fi
+  if gum confirm "Всё верно?"; then
+    clear
+    echo "Начинается установка, откиньтесь на спинку кресла и наслаждайтесь видом :)" | clolcat
+    sleep 2
+    echo -e "\e[34mРазметка дисков..."
+    echo "label: gpt" |  sfdisk "$disk_system"
+    echo "start=        2048, size=     1048576, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B" | sfdisk "$disk_system"
+    echo "start=     1050624, size=    8388608, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4" | sfdisk "$disk_system" -N 3
+    echo "start=     9439232, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4" | sfdisk "$disk_system" -N 2
+    if [ -n "$disk_games" ]; then
+      echo "label: gpt" | sfdisk "$disk_games"
+      echo "type=0FC63DAF-8483-4772-8E79-3D69D8477DE4" | sfdisk "$disk_games"
+    fi
+    echo -e "\e[34mФорматирование и монтирование разделов...\e[0m"
+     mkdir -p /mnt
+    if [ $(echo "$disk_system" | grep -c nvme) -eq 1 ]; then
+       mkfs.fat -n boot -F 32 "''${disk_system}p1"
+       mkfs.btrfs -f -L nixos "''${disk_system}p2"
+       mount "''${disk_system}p2" /mnt
+       btrfs subvolume create /mnt/root
+       btrfs subvolume create /mnt/home
+       btrfs subvolume create /mnt/nix
+       umount /mnt
+       mount -o compress-force=zstd,subvol=root "''${disk_system}p2" /mnt
+       mkdir /mnt/{home,nix}
+       mount -o compress-force=zstd,subvol=home "''${disk_system}p2" /mnt/home
+       mount -o compress-force=zstd,noatime,subvol=nix "''${disk_system}p2" /mnt/nix
+       mkdir /mnt/boot
+       mount "''${disk_system}p1" /mnt/boot
+       mkswap -L swap "''${disk_system}p3"
+       swapon "''${disk_system}p3"
+    else
+       mkfs.fat -n boot -F 32 "''${disk_system}1"
+       mkfs.btrfs -f -L nixos "''${disk_system}2"
+       mount "''${disk_system}2" /mnt
+       btrfs subvolume create /mnt/root
+       btrfs subvolume create /mnt/home
+       btrfs subvolume create /mnt/nix
+       umount /mnt
+       mount -o compress-force=zstd,subvol=root "''${disk_system}2" /mnt
+       mkdir /mnt/{home,nix}
+       mount -o compress-force=zstd,subvol=home "''${disk_system}2" /mnt/home
+       mount -o compress-force=zstd,noatime,subvol=nix "''${disk_system}2" /mnt/nix
+       mkdir /mnt/boot
+       mount "''${disk_system}1" /mnt/boot
+       mkswap -L swap "''${disk_system}3"
+       swapon "''${disk_system}3"
+    fi
+    if [ -n "$disk_games" ]; then
+      if [ $(echo "$disk_games" | grep -c nvme) -eq 1 ]; then
+         mkdir /mnt1
+         mkfs.btrfs -f -L Games "''${disk_games}p1"
+         mount "''${disk_games}p1" /mnt1
+         btrfs subvolume create /mnt1/games
+         umount /mnt1
+         rmdir /mnt1
+      else
+         mkdir /mnt1
+         mkfs.btrfs -f -L Games "''${disk_games}1"
+         mount "''${disk_games}1" /mnt1
+         btrfs subvolume create /mnt1/games
+         umount /mnt1
+         rmdir /mnt1
+      fi
+    fi
+    echo -e "\e[34mУстановка системы...\e[0m"
+      mkdir -p /mnt/etc/nixos
+      rm -rf /mnt/etc/nixos/*
+      rm ./nixos/hardware-configuration.nix
+      nixos-generate-config --no-filesystems --root /mnt
+      find /mnt/etc/nixos ! -name 'hardware-configuration.nix' -type f -exec rm -rf {} +
+      cp -r ./nixos ./iso flake.{nix,lock} /mnt/etc/nixos
+      mv /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/nixos
+      nixos-install -v --flake "/mnt/etc/nixos#nixos" --impure
+    echo "\e[32mУстановка завершена, перезагрузка через 10 секунд... (Ctrl+C для отмены)\e[0m"
+    for i in {1..9}; do
+      sleep 0.25
+      printf "${i}"
+      sleep 0.25
+      printf "."
+      sleep 0.25
+      printf "."
+      sleep 0.25
+      printf "."
+    done
+    sleep 0.25
+    printf "10"
+    echo -ne '\n'
+    reboot
+  fi
 else
   echo "change your working directory to dotfiles"
 fi
@@ -14,11 +113,11 @@ fi
 ##!/usr/bin/env bash
 #if [ -f ./check ] &; then
 #  git clone https://github.com/DADA30000/mozilla.git ~/.mozilla
-#  sudo touch /password
-#  ( echo "Введите пароль Nextcloud"; read; sudo chmod 777 /password; echo $REPLY >> /password; sudo chown nextcloud:nextcloud /password; sudo chmod 400 /password )
-#  sudo mkdir /fileserver
+#   touch /password
+#  ( echo "Введите пароль Nextcloud"; read;  chmod 777 /password; echo $REPLY >> /password;  chown nextcloud:nextcloud /password;  chmod 400 /password )
+#   mkdir /fileserver
 #  git config --global credential.helper store
-#  sudo chown -R nginx:nginx /fileserver
+#   chown -R nginx:nginx /fileserver
 #else
 #  echo "change your working directory to dotfiles"
 #fi
