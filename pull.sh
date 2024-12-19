29c6a0c303f6e1c645603dee1d2b849bfadeb9f5
DADA30000 <vadimhack.ru@gmail.com>
2024-12-20 02:17:54 +0300

    lil update

--- pull.sh
+++ pull.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 if [ -f ./check ]; then
-  sudo mv /etc/nixos/stuff/singbox/config /tmp/config-aaaa
+  sudo mv /etc/nixos/stuff/singbox/config.json /tmp/config-aaaa
   git pull
   sudo rm -rf /etc/nixos/*
+  rm ./stuff/repo.tar.gz
   sudo cp -r ./machines ./modules ./stuff ./flake.lock ./flake.nix /etc/nixos/
-  sudo mv /tmp/config-aaaa /etc/nixos/stuff/singbox/config
+  sudo mv /tmp/config-aaaa /etc/nixos/stuff/singbox/config.json
 else
   echo "change your working directory to dotfiles already"
 fi
