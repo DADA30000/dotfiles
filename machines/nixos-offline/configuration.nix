e46c092e22ab5cc11e85e105d61975116de3770c
DADA30000 <vadimhack.ru@gmail.com>
2025-05-17 20:25:05 +0300

    finallllllyyyyyyyyyyy

--- machines/nixos-offline/configuration.nix
+++ machines/nixos-offline/configuration.nix
@@ -5,11 +5,15 @@
   user_iso,
   ...
 }:
+let
+  orig = inputs.self.outputs.nixosConfigurations.nixos.config.users.users;
+  users_without = lib.removeAttrs orig [ "l0lk3k" ];
+  imported = inputs.self.outputs.nixosConfigurations.nixos.config.users.users."${user}";
+  changed = lib.mkMerge [ imported { hashedPassword = lib.mkForce null; initialPassword = lib.mkForce "1234"; }];
+in
 {
   imports = [ ../../machines/nixos/configuration.nix ];
-  users.users."${user}" = lib.mkForce {};
-  users.users."${user_iso}" = lib.mkMerge [
-    inputs.self.outputs.nixosConfigurations.nixos.config.users.users."${user}"
-    { hashedPassword = lib.mkForce null; initialPassword = "1234"; }
-  ];
+  users.users = lib.mkForce (
+    users_without // { "${user_iso}" = changed; }
+  );
 }
