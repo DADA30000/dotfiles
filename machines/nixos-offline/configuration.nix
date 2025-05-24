9f2811752a38ba8ca995506a114dfa54bbed2364
DADA30000 <vadimhack.ru@gmail.com>
2025-05-24 12:35:27 +0300

    fix

--- machines/nixos-offline/configuration.nix
+++ machines/nixos-offline/configuration.nix
@@ -7,7 +7,7 @@
 }:
 let
   orig = inputs.self.outputs.nixosConfigurations.nixos.config.users.users;
-  users_without = lib.removeAttrs orig [ "l0lk3k" ];
+  users_without = lib.removeAttrs orig [ user ];
   imported = inputs.self.outputs.nixosConfigurations.nixos.config.users.users."${user}";
   changed = lib.mkMerge [ imported { hashedPassword = lib.mkForce null; initialPassword = lib.mkForce "1234"; }];
 in
