fab306bf5847e3509f46b12e4dede49c26806152
DADA30000 <vadimhack.ru@gmail.com>
2025-06-26 19:11:53 +0300

    fix ISO

--- machines/iso8G/configuration.nix
+++ machines/iso8G/configuration.nix
@@ -10,7 +10,7 @@
   environment.systemPackages = lib.mkForce (
     lib.filter (
       x: x != inputs.zen-browser.packages.${pkgs.system}.twilight && x != pkgs.qemu && x != pkgs.rustc && x != pkgs.cargo && x != pkgs.ccls && x != pkgs.rust-analyzer && x != pkgs.speechd && x != pkgs.speechd-minimal
-    ) inputs.self.outputs.nixosConfigurations.nixos.config.environment.systemPackages
+    ) inputs.self.outputs.nixosConfigurations.iso.config.environment.systemPackages
   );
   home-manager.users."${user}" = import ./home.nix;
   obs.enable = lib.mkForce false;
