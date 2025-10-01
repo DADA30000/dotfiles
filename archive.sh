87dbf68e77cf7efc44f635aa3cc79fade5604456
DADA30000 <vadimhack.ru@gmail.com>
2025-10-01 20:46:25 +0300

    lil update

--- archive.sh
+++ archive.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 if [ -f ./check ]; then
   git rm ./stuff/repo.tar.gz
+  git rm ./stuff/nixpkgs.tar.zst
+  rm ./stuff/nixpkgs.tar.zst
   rm ./stuff/repo.tar.gz
   tar --exclude=repo.tar.gz --exclude=nixpkgs.tar.zst --exclude=*.mp4 -czvf ./stuff/repo.tar.gz ./check ./flake.lock ./flake.nix ./machines ./modules ./pull.sh ./README.md ./screenshot.png ./start.sh ./stuff ./update.sh ./archive.sh
 else
