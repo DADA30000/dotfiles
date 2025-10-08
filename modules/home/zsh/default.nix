44410e71e61520b32a848ac08d42af5a2098234a
DADA30000 <vadimhack.ru@gmail.com>
2025-10-08 23:54:02 +0300

    ultimate completion update

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -56,51 +56,131 @@ in
               export MANPAGER='nvim +Man!'
               printf '\n%.0s' {1..100}
               setopt correct
-              _ns_completer () {
-                local attr_prefix="$1"
-                shift words
-                words=(nix shell $words)
-                (( CURRENT++ ))
+              _ns_completer() {
+                local subcommand
+                subcommand="$1"
+                local attr_prefix
+                attr_prefix="$2"
                 local -a processed_words
                 for word in "''${words[@]}"; do
-                  if [[ "$word" == -* || "$word" == "nix" || "$word" == "shell" || "$word" == "develop" ]]; then
+                  if [[ "$word" == -* || "$word" == "nix" || "$word" == "shell" || "$word" == "develop" || "$word" == "eval" ]]; then
                     processed_words+=("$word")
                   else
                     processed_words+=("''${attr_prefix}$word")
                   fi
                 done
-                words=("''${processed_words[@]}")
-                local ifs_bk="$IFS" 
-                local input=("''${(Q)words[@]}") 
-                IFS=$'\n' 
-                local res=($(NIX_GET_COMPLETIONS=$((CURRENT - 1)) "$input[@]" 2>/dev/null)) 
-                IFS="$ifs_bk" 
-                local tpe="''${''${res[1]}%%>	*}" 
-                local -a suggestions
-                declare -a suggestions
-                for suggestion in ''${res:1}
-                do
-                	suggestions+=("''${suggestion%%	*}") 
-                done
-                suggestions=("''${(@)suggestions/#''${attr_prefix}/}")
-                local -a args
-                if [[ "$tpe" == filenames ]]
-                then
-                	args+=('-f') 
-                elif [[ "$tpe" == attrs ]]
-                then
-                	args+=('-S' ''') 
+                local curr_word
+                curr_word="''${processed_words[$CURRENT]}"
+                if [[ ! "$curr_word" == -* ]]; then
+                  if [[ -n "$curr_word" ]]; then
+                    if [[ "$curr_word" == *.* ]]; then
+                      local packages_string
+                      packages_string=$(nix eval --raw --expr "
+                        let
+                          pkgs = import (builtins.getFlake \"git+file://$NIX_PATHH?rev=${inputs.nixpkgs.rev}&shallow=1\") {
+                            system = \"x86_64-linux\";
+                            config.allowUnfree = true;
+                          };
+                          startAttr = pkgs.''${curr_word%.*};
+                          startPrefix = \"''${curr_word%.*}\";
+                          childNames = builtins.attrNames startAttr;
+                          formattedNames = map (name: \"\''${startPrefix}.\''${name}\") childNames;
+                        in
+                        builtins.concatStringsSep \"\n\" formattedNames
+                      " 2>/dev/null)
+                      local -a packages
+                      packages=(''${(f)packages_string})
+                    elif [[ -f /tmp/nix_completer_cache ]]; then
+                      local -a packages
+                      packages=( ''${(f)"$(</tmp/nix_completer_cache)"} )
+                    else
+                      local packages_string
+                      packages_string=$(nix eval --raw --expr "
+                        builtins.concatStringsSep \"\n\" (
+                          builtins.attrNames (
+                            import (builtins.getFlake \"git+file://$NIX_PATHH?rev=${inputs.nixpkgs.rev}&shallow=1\") {
+                              system = \"x86_64-linux\";
+                              config.allowUnfree = true;
+                            }
+                          )
+                        )
+                      " 2>/dev/null)
+                      print -rn -- "$packages_string" > /tmp/nix_completer_cache
+                      local -a packages
+                      packages=(''${(f)packages_string})
+                    fi
+              
+                    local -a suggestions_first
+                    suggestions_first=( ''${(M)packages:#"$curr_word"*} )
+                    local -a suggestions_second
+                    suggestions_second=( ''${(M)packages:#*"$curr_word"*} )
+              
+                    typeset -A seen
+                    for item in "''${suggestions_first[@]}"; do
+                      seen[$item]=1
+                    done
+              
+                    local -a suggestions_second_unique
+                    suggestions_second_unique=()
+                    for item in "''${suggestions_second[@]}"; do
+                      if [[ -z "''${seen[$item]}" ]]; then
+                        suggestions_second_unique+=("$item")
+                      fi
+                    done
+              
+                    local -a suggestions
+                    suggestions=( ''${suggestions_first[@]} ''${suggestions_second_unique[@]} )
+                    suggestions=( "''${(@)suggestions/#''${attr_prefix}/}" )
+                    compadd -a suggestions
+                  fi
+                else
+                  shift processed_words
+                  local -a proc_words
+                  for word in "''${processed_words[@]}"; do
+                    if [[ "$word" == -* ]]; then
+                      proc_words+=("$word")  
+                    fi
+                  done
+                  proc_words=(nix $subcommand $proc_words)
+                  local ifs_bk="$IFS" 
+                  local input=("''${(Q)proc_words[@]}") 
+                  IFS=$'\n' 
+                  local res=($(NIX_GET_COMPLETIONS=$((CURRENT)) "$input[@]" 2>/dev/null)) 
+                  IFS="$ifs_bk" 
+                  local tpe="''${''${res[1]}%%>	*}" 
+                  local -a suggestions
+                  declare -a suggestions
+                  for suggestion in ''${res:1}
+                  do
+                  	suggestions+=("''${suggestion%%	*}") 
+                  done
+                  local -a args
+                  if [[ "$tpe" == filenames ]]
+                  then
+                  	args+=('-f') 
+                  elif [[ "$tpe" == attrs ]]
+                  then
+                  	args+=('-S' ''') 
+                  fi
+                  compadd -J nix "''${args[@]}" -a suggestions
                 fi
-                compadd -J nix "''${args[@]}" -a suggestions
               }
               _ns-py () {
-                _ns_completer nixpkgs\#python3Packages.
+                _ns_completer shell python3Packages.
               }
               _ns () {
-                _ns_completer nixpkgs\#
+                _ns_completer shell
               }
-              compdef _ns ns ns-dev
+              _ns-dev () {
+                _ns_completer develop
+              }
+              _ns-eval () {
+                _ns_completer eval
+              }
+              compdef _ns ns
+              compdef _ns-dev ns-dev
               compdef _ns-py ns-py
+              compdef _ns-eval ns-eval
               # Ad-hoc python with modules env
               ns-py () {
                 local flags=()
@@ -130,6 +210,16 @@ in
                 done
               
                 nix shell "''${flags[@]}" --expr "with import (builtins.getFlake \"git+file://$NIX_PATHH?rev=${inputs.nixpkgs.rev}&shallow=1\") { system = \"${pkgs.system}\"; config.allowUnfree = true; }; [ ''${pkgs[*]} ]"
+              }
+              # ad-hoc nix eval expr
+              ns-eval () {
+                local flags=()
+                local pkgs=()
+                for arg in "$@"; do
+                  [[ "$arg" == -* ]] && flags+=("$arg") || pkgs+=("($arg)")
+                done
+              
+                nix eval "''${flags[@]}" --expr "builtins.concatStringsSep \"\n\" (with import (builtins.getFlake \"git+file://$NIX_PATHH?rev=${inputs.nixpkgs.rev}&shallow=1\") { system = \"${pkgs.system}\"; config.allowUnfree = true; }; [ ''${pkgs[*]} ])"
               }
             '';
             zshEarly = mkOrder 500 ''
