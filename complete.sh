2d8b1b79eae715b6f7d5ce841e361914293f156d
DADA30000 <vadimhack.ru@gmail.com>
2025-10-21 01:16:05 +0300

    lil update

--- complete.sh
+++ complete.sh
@@ -2,7 +2,6 @@
 if [[ -f ./check ]]; then
   if [[ ! -f ./stuff/nixpkgs.tar.zst ]]; then
     cat stuff/nixpkgs.tar.zst.part* > ./stuff/nixpkgs.tar.zst
-    git add ./stuff/nixpkgs.tar.zst
   fi
 else
   echo "change your working directory to dotfiles already"
