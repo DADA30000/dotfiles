c70c08172d5dd5a2d2d4b37c8c497a6d6f78d9ca
DADA30000 <vadimhack.ru@gmail.com>
2025-12-08 23:13:24 +0300

    lil update

--- machines/iso/home.nix
+++ machines/iso/home.nix
@@ -1,11 +1,6 @@
 { lib, pkgs, ... }:
 {
 
-  # Import original home.nix
-  imports = [ ../../machines/nixos/home.nix ];
-
-  firefox.enable = lib.mkForce true;
-
   flatpak.enable = lib.mkForce false;
 
   systemd.user.services.umu-check.Install.WantedBy = lib.mkForce [ ];
