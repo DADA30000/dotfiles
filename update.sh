27fb699fb0997499bab707c8a2f2e597d5791071
DADA30000 <vadimhack.ru@gmail.com>
2026-04-28 04:03:40 +0300

    cleanup

--- update.sh
+++ update.sh
@@ -3,7 +3,6 @@ if [ -f ./check ]; then
   echo "Critical step, do not terminate script now"
   rm -rf ./machines ./modules ./stuff ./flake.nix ./flake.lock
   cp -r /etc/nixos/* ./
-  ./archive.sh
   git add . --all
   echo "You can terminate now"
   printf "Enter commit name (enter to default): "
